b48ee2e7b7b265d700a861f32d14a9fa8bdc1e11
guanqun <15800349672@163.com>
2022-12-06 09:31:22 +0800

    --

--- dayHighGapBuyStrategy/dayHighGapBuyStrategy.js
+++ dayHighGapBuyStrategy/dayHighGapBuyStrategy.js
@@ -2,10 +2,10 @@ var $dayHighGapBuyStrategy = {
 	data:function() {
 		return {
 			excelTables:[], // Excel 数据保存对象
-			
 			// 页面渲染数据对象 
-			renderList: [],
+			renderList: [{"code":"002932","name":"明德生物","date":"2022-11-22","diffRate":"-5.82","openPrice":"73.00","maxPrice":"73.00","closePrice":"67.00","strategyRate":33,"risePlateCount":1,"yesterdayClose":"71.14","_todayClose":"61.05","_todayInYesterdayClose":"66.56","_strategyPositionPrices":"64.64","_floatStrategyPositionPrices":"64.75","_afterDiffRate":"5.88","_currentRate":"-8.28","_coreHighRate":"2.61"},{"code":"002187","name":"广百股份","date":"2022-12-02","diffRate":"-2.56","openPrice":"8.99","maxPrice":"9.84","closePrice":"8.75","strategyRate":33,"risePlateCount":2,"yesterdayClose":"8.98","_todayClose":"8.01","_todayInYesterdayClose":"8.75","_strategyPositionPrices":"7.44","_floatStrategyPositionPrices":"7.45","_afterDiffRate":"-7.12","_currentRate":"-8.46","_coreHighRate":"4.84"}],
 			
+			selectDaySize: 10, // 查询数据的范围
 			noMainBoard: 0, // 非主板个股数量
 		}
 	},
@@ -18,22 +18,48 @@ var $dayHighGapBuyStrategy = {
 			var params = "code=" + item.code + '&beginTime=' + beginTime + '&onceDay=true';
 			window.open('../volPriceAnalysis/量价计算研测.html?' + params);
 		},
-		//  列表策略位率变更事件
-		eStrategyInputChange:function(event, index) {
+		//  列表策略位率变更触发
+		listStrategyRateChange:function(event, index) {
 			this.renderList[index].strategyRate = event.target.value;
+		},
+		// 列表涨板位变更触发
+		listRisePlateCountChange:function(event, index) {
+			this.renderList[index].risePlateCount = event.target.value;
+		},
+		// 剩余差幅
+		afterDiffRate:function(item) {
+			var price = this.excelStrategyPositionPrices(item);
+			return this.diffRateCompute(price, item._todayClose)
+		},
+		// 浮动策略位价
+		floatStrategyPositionPrices:function(item) {
+			var strategyPrice = this.strategyPositionPriceTwo(item, item.strategyRate, item.risePlateCount);
+			var result = Number(strategyPrice) + (strategyPrice * 0.166 / 100)
+			return (result || 0).toFixed(2);
 		},
 		// 列表策略位价计算
 		excelStrategyPositionPrices:function(item) {
-			if(item.yesterdayClose && item.openPrice) {
-				var opRate = this.diffRateCompute(item.openPrice, item.yesterdayClose);
-				var strategyRate = opRate - (opRate * item.strategyRate / 100);
-				
-				// 回归位价
-				var flybackPrice = item.openPrice - (item.openPrice * opRate / 100);
-				// 策略位价
-				var strategyPrice = flybackPrice - (flybackPrice * strategyRate / 100);
-				return strategyPrice.toFixed(2);
+			return this.strategyPositionPriceTwo(item, item.strategyRate, item.risePlateCount);
+		},
+		// 获取连续涨板的次数
+		getRisePlateCount:function(dataList, date) {
+			var _risePlateCount = 1;
+			var eachCounts = function(listIndex) {
+				var diffRate = dataList[listIndex].diffRate;
+				if(diffRate && Number(diffRate) >= 9.9) {
+					_risePlateCount += 1;
+					eachCounts(listIndex + 1);
+				}
 			}
+			
+			for(var index = 0;index < dataList.length;index++) {
+				var item = dataList[index];
+				if(moment(item.date).format('YYYY-MM-DD') == date) {
+					eachCounts(index + 1);
+					break;
+				}
+			}
+			return _risePlateCount;
 		},
 		// 迭代器
 		iterator:function(object, callback) {
@@ -41,28 +67,45 @@ var $dayHighGapBuyStrategy = {
 			var params = {
 				code: object.code,
 				time: 'day',
-				beginDay: moment(object.date).subtract(3, 'days').format('YYYYMMDD'),
-			}
+				beginDay: moment().subtract(_this.selectDaySize, 'days').format('YYYYMMDD'),
+			};
 			
-			var renderObject = new Object();
 			_this.fetchStockRealKlineData(params,function(data) {
-				var dataList = data.dataList.reverse();
+				var dataList = data.dataList;
+				dataList.forEach(function(item, index) {
+					if(index != dataList.length - 1) {
+						var yesterdayClose = dataList[index + 1].close; // 昨日收盘价
+						item.diffRate = _this.diffRateCompute(item.close, yesterdayClose); // 当日涨跌幅
+						item.date = moment(item.time).format('YYYY-MM-DD');
+					}
+				})
+				
+				var renderObject = new Object();
 				for(var i = 0;i < dataList.length;i++) {
 					var item = dataList[i];
-					if(item['time'] == moment(object.date).format('YYYYMMDD')) {
+					if(i != 0 && item.diffRate && Number(item.diffRate) >= 9.9) {
+						// 涨板次数
+						var risePlateCount = _this.getRisePlateCount(dataList, item.date);
+						
 						Object.assign(renderObject, {
 							code: object.code,
 							name: object.name,
-							openPrice: item.open,
-							maxPrice: item.max,
-							minPrice: item.min,
-							closePrice: item.close,
-							strategyRate: 33,
-							date: object.date,
+							plate: object.plate,
+							date: dataList[i - 1].date,
+							diffRate: dataList[i - 1].diffRate,
 							
-							yesterdayClose: dataList[i - 1].close,
-							yesterdayDate: dataList[i - 1].date,
+							openPrice: dataList[i - 1].open,
+							maxPrice: dataList[i - 1].max,
+							closePrice: dataList[i - 1].close,
+							strategyRate: 33,
+							risePlateCount: risePlateCount,
+							yesterdayClose: item.close,
+							
+							_todayClose: dataList[0].close,
+							_todayInYesterdayClose: dataList[1].close,
 						});
+						
+						console.log(JSON.stringify(renderObject))
 						_this.renderList.push(renderObject);
 						break;
 					}
@@ -85,11 +128,15 @@ var $dayHighGapBuyStrategy = {
 					_this.iterationFetch(eachIndex);
 				});
 			}else {
+				// 结束数据处理
+				for(var item of _this.renderList) {
+					// 剩余差幅
+					item._afterDiffRate = _this.afterDiffRate(item);
+				}
+				
 				// Array Sort
 				this.renderList.sort(function(v1, v2) {
-					var diffRateCompute_v1 = _this.diffRateCompute(v1.closePrice, v1.yesterdayClose);
-					var diffRateCompute_v2 = _this.diffRateCompute(v2.closePrice, v2.yesterdayClose);
-					return diffRateCompute_v1 - diffRateCompute_v2;
+					return v2._afterDiffRate - v1._afterDiffRate;
 				})
 				
 				// console.log(JSON.stringify(_this.renderList))
@@ -99,23 +146,23 @@ var $dayHighGapBuyStrategy = {
 		handleFiles:function(input) {
 			var _this = this;
 			var files = (input || document.getElementById("excel_file") ).files;
+			if(files.length == 0) { return };
 			
 			var eachReadyFile = function() {
 				let reader = new FileReader();
 				reader.onload = function(e) {
 					var results = e.target.result.split('\n');
-					var date = results[0].replace(/\r/g, '');
-					var titles = results[1].replace(/\s/g, '').split(',');
+					var titles = results[0].replace(/\s/g, '').split(',');
 					var stocks = results.filter(function(item, index) {
-						// 0: Date, 1: Header
-						if(item && index != 0 && index != 1) { 
+						if(item && index != 0) { 
 							return item.split(',')
 						};
 					});
 					
 					var keys = {
 						"自选股": { key: 'name' },
-						"代码": { key: 'code' }
+						"代码": { key: 'code' },
+						"行业": { key: 'plate' }
 					}
 					
 					Object.keys(keys).forEach(function(keyname) {
@@ -141,7 +188,6 @@ var $dayHighGapBuyStrategy = {
 					var newList = stockList.filter(function(item) {
 						var code = item.code;
 						if(['6', '0'].includes(code.substr(0, 1))) {
-							item.date = date;
 							return item;
 						}else {
 							_this.noMainBoard += 1;
@@ -149,7 +195,7 @@ var $dayHighGapBuyStrategy = {
 						}
 					});
 					
-					// console.log(newList)
+					// console.log(JSON.stringify(newList));
 					
 					_this.excelTables = newList;
 					_this.iterationFetch();
